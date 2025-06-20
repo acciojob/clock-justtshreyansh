6b79592a7f25d5823e2bb31c65f29dd472b05445
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-20 06:18:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const p = document.querySelector("#timer");
+function updateTime(){
+	const date = new Date();
+	p.textContent = date.toLocaleString();
+}
+updateTime();
+setInterval(updateTime,1000);
+
